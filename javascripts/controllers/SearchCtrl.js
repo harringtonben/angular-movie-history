70e66b27dc280a07340eeac15db45f952f77a497
ben harrington <harringtonben@icloud.com>
2017-11-14 20:33:52 -0600

    finished image card

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,11 +1,12 @@
 'use strict';
 
 app.controller("SearchCtrl", function($scope, tmdbService){
-    
+    $scope.movies = [];
+
     $scope.enterPush = (event) => {
         if (event.keyCode === 13) {
             tmdbService.searchMovies(event.target.value).then((results) => {
-                console.log(results.data.results);
+                $scope.movies = results.data.results;
             }).catch((error) => {
                 console.log("Error in searchMovies", error);
             });
