5c054bdbb8ceeb21800facce33227bbff622badc
ben harrington <harringtonben@icloud.com>
2017-11-15 20:29:09 -0600

    finished wishlist

--- javascripts/services/MovieService.js
+++ javascripts/services/MovieService.js
@@ -21,7 +21,26 @@ app.service("MovieService", function($q, $http, FIREBASE_CONFIG) {
         });
     };
 
-    return {getRatedMovies};
+    const getWishlistMovies = (userUid) => {
+        let movies = [];
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/movies.json?orderBy="uid"&equalTo="${userUid}"`).then((results) => {
+                let fbMovies = results.data;
+                if (fbMovies != null) {
+                    Object.keys(fbMovies).forEach((key) => {
+                        fbMovies[key].id = key;
+                        if (!fbMovies[key].isWatched) {
+                            movies.push(fbMovies[key]);
+                        }
+                        resolve(movies); 
+                    });    
+                }
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    };
+    return {getRatedMovies, getWishlistMovies};
 });
 
 
