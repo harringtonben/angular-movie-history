a78cc7f5884cef41fff34407e8db8cae6c831990
ben harrington <harringtonben@icloud.com>
2017-11-15 21:30:00 -0600

    finished delete functionality

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -2,10 +2,22 @@
 
 app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
     $scope.movies = [];
+
+    const getMovies = () => {
+        MovieService.getWishlistMovies($rootScope.uid).then((results) => {
+            $scope.movies = results;
+        }).catch((error) => {
+            console.log("error in getRatedMovies", error);
+        });
+    };
+
+    getMovies();
     
-    MovieService.getWishlistMovies($rootScope.uid).then((results) => {
-        $scope.movies = results;
-    }).catch((error) => {
-        console.log("error in getRatedMovies", error);
-    });
+    $scope.deleteMovie = (movieId) => {
+        MovieService.deleteMovie(movieId).then((result) => {
+            getMovies();
+        }).catch((error) => {
+            console.log("error in getRatedMovies", error);
+        });
+    };
 });
